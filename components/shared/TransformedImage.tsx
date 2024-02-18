f7a87036d253fc83aa1772e435b5af8cc4d4839a
YuvrajTak <yuvrajtak17@gmail.com>
2024-02-18 16:14:31 +0530

    photo collection web site initial commit

---
+++ components/shared/TransformedImage.tsx
@@ -0,0 +1,14 @@
+import { dataUrl, debounce, download, getImageSize } from '@/lib/utils'
+import { PlaceholderValue } from 'next/dist/shared/lib/get-img-props'
+import Image from 'next/image'
+
+import React from 'react'
+
+const TransformedImage = ({image, type, title, transformationConfig, isTransforming, setIsTransforming, hasDownload = false}
+     : TransformedImageProps) => {
+  return (
+    <div>TransformedImage</div>
+  )
+}
+
+export default TransformedImage
